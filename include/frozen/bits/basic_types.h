81069054e2209b0477f31a1b251f002ab2616a01
Adam Shapiro <adam@pointonenav.com>
2025-05-28 14:51:33 -0400

    Restored default element construction in cvector class.
    With or without ={}, this works correctly for most complex objects since the
    compiler will always call its default constructor no matter what. However, if
    the object defines other constructors and does not define a default constructor,
    no default constructor exists. For primitives, there is no such thing as a
    default constructor, and the compiler is not guaranteed to initialize the value
    if there is no explicit initialization. So omitting ={} is technically undefined
    behavior for primitives, and guaranteed to fail for complex objects with no
    default constructor.
    
    When a cvector is used within a constexpr function/context, since the cvector
    construction _may_ have undefined behavior, the compiler may fail in weird ways.
    In this case, when using frozen::unordered_map it failed with:
    
    external/frozen/include/frozen/unordered_map.h:86:48: error: 'constexpr
    frozen::bits::pmh_tables<M, Hash> frozen::bits::make_pmh_tables(const
    frozen::bits::carray<Item, N>&, const Hash&, const Key&, PRG) [with long
    unsigned int M = 32; Item =
    std::pair<point_one::fusion_engine::messages::SatelliteType, char>; long
    unsigned int N = 9; Hash = EnumClassHash; Key = frozen::bits::GetKey; PRG =
    frozen::linear_congruential_engine<long unsigned int, 48271, 0, 2147483647>]'
    called in a constant expression
       86 |             bits::make_pmh_tables<storage_size>(
          |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
       87 |                 items_, hash, bits::GetKey{}, default_prg_t{})} {}
          |                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    In file included from external/frozen/include/frozen/unordered_map.h:30,
                     from point_one/common/satellite_type.cc:14:
    external/frozen/include/frozen/bits/pmh.h:171:31: note: 'constexpr
    frozen::bits::pmh_tables<M, Hash> frozen::bits::make_pmh_tables(const
    frozen::bits::carray<Item, N>&, const Hash&, const Key&, PRG) [with long
    unsigned int M = 32; Item =
    std::pair<point_one::fusion_engine::messages::SatelliteType, char>; long
    unsigned int N = 9; Hash = EnumClassHash; Key = frozen::bits::GetKey; PRG =
    frozen::linear_congruential_engine<long unsigned int, 48271, 0, 2147483647>]' is
    not usable as a 'constexpr' function because:
      171 | pmh_tables<M, Hash> constexpr make_pmh_tables(const carray<Item, N> &
          |                               ^~~~~~~~~~~~~~~
    external/frozen/include/frozen/bits/pmh.h:191:3: error: 'goto' is not a constant
    expression
      191 |   for (const auto & bucket : buckets) {

--- include/frozen/bits/basic_types.h
+++ include/frozen/bits/basic_types.h
@@ -44,7 +44,7 @@ class cvector {
   // Make sure custom `FROZEN_SIZE_T` is big enough for templated usage.
   static_assert(std::numeric_limits<FROZEN_SIZE_T>::max() >= N);
 
-  T data_[N];
+  T data_[N] = {};
   FROZEN_SIZE_T dsize_ = 0;
 
   template <class Iter>
